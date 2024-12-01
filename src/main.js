6fe98a6f87b3db21bb0499a53656aef1922c45a3
Jasper <3108316809@qq.com>
2024-12-01 22:20:43 +0800

    Deploy to GitHub Pages

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router/index.js';
-
- createApp(App).use(router).mount('#app');
-
-//调试
-console.log("Vue App Mounted");
